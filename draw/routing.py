c8029e7daa53147deb0adf468949c9b43dd6b0a3
forrest_huang <forrest_huang@berkeley.edu>
2019-07-14 20:57:09 -0400

    Removed / at the end of url

--- draw/routing.py
+++ draw/routing.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-   url(r'^ws/draw/$', consumers.DrawConsumer),
+   url(r'^ws/draw$', consumers.DrawConsumer),
 ]
